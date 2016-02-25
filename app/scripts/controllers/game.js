69fe6fbe8a86e7571a8885dba77ad7e2a48799a5
Ihab Khattab <ikhattab.mail@gmail.com>
2016-02-25 19:56:17 +0200

    added read

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,17 +1,19 @@
 angular
   .module('game')
-  .controller('MainController', function(gameService, $timeout) {
-    var vm = this;
-    vm.startGame = function(choice) {
-      vm.computerChoice = null;
-      vm.userChoice = null;
-      vm.result = null;
-      vm.animate = true;
-      $timeout(function() {
-        vm.animate = false;
-        vm.userChoice = choice;
-        vm.computerChoice = gameService.getRandomChoice();
-        vm.result = gameService.getTheWinner(vm.computerChoice, vm.userChoice);
-      }, 1500);
-    };
-  });
+  .controller('MainController', MainController);
+
+function MainController(gameService, $timeout) {
+  var vm = this;
+  vm.startGame = function(choice) {
+    vm.computerChoice = null;
+    vm.userChoice = null;
+    vm.result = null;
+    vm.animate = true;
+    $timeout(function() {
+      vm.animate = false;
+      vm.userChoice = choice;
+      vm.computerChoice = gameService.getRandomChoice();
+      vm.result = gameService.getTheWinner(vm.computerChoice, vm.userChoice);
+    }, 1500);
+  };
+}
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -30,7 +30,10 @@ import del from 'del';
 import runSequence from 'run-sequence';
 import browserSync from 'browser-sync';
 import gulpLoadPlugins from 'gulp-load-plugins';
-import { Server as Karma } from 'karma';
+import {
+  Server as Karma
+}
+from 'karma';
 import pkg from './package.json';
 
 const $ = gulpLoadPlugins();
@@ -39,20 +42,22 @@ const reload = browserSync.reload;
 // Lint JavaScript
 gulp.task('lint', () =>
   gulp.src('app/scripts/**/*.js')
-    .pipe($.eslint())
-    .pipe($.eslint.format())
-    .pipe($.if(!browserSync.active, $.eslint.failOnError()))
+  .pipe($.eslint())
+  .pipe($.eslint.format())
+  .pipe($.if(!browserSync.active, $.eslint.failOnError()))
 );
 
 // Optimize images
 gulp.task('images', () =>
   gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
-    .pipe(gulp.dest('dist/images'))
-    .pipe($.size({title: 'images'}))
+  .pipe($.cache($.imagemin({
+    progressive: true,
+    interlaced: true
+  })))
+  .pipe(gulp.dest('dist/images'))
+  .pipe($.size({
+    title: 'images'
+  }))
 );
 
 // Copy all files at the root level (app)
@@ -63,7 +68,9 @@ gulp.task('copy', () =>
   ], {
     dot: true
   }).pipe(gulp.dest('dist'))
-    .pipe($.size({title: 'copy'}))
+  .pipe($.size({
+    title: 'copy'
+  }))
 );
 
 // Compile and automatically prefix stylesheets
@@ -82,9 +89,9 @@ gulp.task('styles', () => {
 
   // For best performance, don't add Sass partials to `gulp.src`
   return gulp.src([
-    'app/styles/**/*.scss',
-    'app/styles/**/*.css'
-  ])
+      'app/styles/**/*.scss',
+      'app/styles/**/*.css'
+    ])
     .pipe($.newer('.tmp/styles'))
     .pipe($.sourcemaps.init())
     .pipe($.sass({
@@ -94,7 +101,9 @@ gulp.task('styles', () => {
     .pipe(gulp.dest('.tmp/styles'))
     // Concatenate and minify styles
     .pipe($.if('*.css', $.cssnano()))
-    .pipe($.size({title: 'styles'}))
+    .pipe($.size({
+      title: 'styles'
+    }))
     .pipe($.sourcemaps.write('./'))
     .pipe(gulp.dest('dist/styles'));
 });
@@ -103,30 +112,35 @@ gulp.task('styles', () => {
 // to enables ES2015 support remove the line `"only": "gulpfile.babel.js",` in the
 // `.babelrc` file.
 gulp.task('scripts', () =>
-    gulp.src([
-      // Note: Since we are not using useref in the scripts build pipeline,
-      //       you need to explicitly list your scripts here in the right order
-      //       to be correctly concatenated
-      './app/scripts/main.js'
-      // Other scripts
-    ])
-      .pipe($.newer('.tmp/scripts'))
-      .pipe($.sourcemaps.init())
-      .pipe($.babel())
-      .pipe($.sourcemaps.write())
-      .pipe(gulp.dest('.tmp/scripts'))
-      .pipe($.concat('main.min.js'))
-      .pipe($.uglify({preserveComments: 'some'}))
-      // Output files
-      .pipe($.size({title: 'scripts'}))
-      .pipe($.sourcemaps.write('.'))
-      .pipe(gulp.dest('dist/scripts'))
+  gulp.src([
+    './app/vendor/angular/angular.js',
+    './app/scripts/main.js',
+    './app/scripts/controllers/game.js',
+    './app/scripts/services/game.js'
+  ])
+  // .pipe($.newer('.tmp/scripts'))
+  // .pipe($.sourcemaps.init())
+  .pipe($.babel())
+  // .pipe($.sourcemaps.write())
+  .pipe(gulp.dest('.tmp/scripts'))
+  .pipe($.concat('main.min.js'))
+  // .pipe($.uglify({
+    // preserveComments: 'some'
+  // }))
+  // Output files
+  // .pipe($.size({
+    // title: 'scripts'
+  // }))
+  // .pipe($.sourcemaps.write('.'))
+  .pipe(gulp.dest('dist/scripts'))
 );
 
 // Scan your HTML for assets & optimize them
 gulp.task('html', () => {
   return gulp.src('app/**/*.html')
-    .pipe($.useref({searchPath: '{.tmp,app}'}))
+    .pipe($.useref({
+      searchPath: '{.tmp,app}'
+    }))
     // Remove any unused CSS
     .pipe($.if('*.css', $.uncss({
       html: [
@@ -136,29 +150,34 @@ gulp.task('html', () => {
       ignore: []
     })))
 
-    // Concatenate and minify styles
-    // In case you are still using useref build blocks
-    .pipe($.if('*.css', $.cssnano()))
+  // Concatenate and minify styles
+  // In case you are still using useref build blocks
+  .pipe($.if('*.css', $.cssnano()))
 
-    // Minify any HTML
-    .pipe($.if('*.html', $.htmlmin({
-      removeComments: true,
-      collapseWhitespace: true,
-      collapseBooleanAttributes: true,
-      removeAttributeQuotes: true,
-      removeRedundantAttributes: true,
-      removeEmptyAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      removeOptionalTags: true
-    })))
-    // Output files
-    .pipe($.if('*.html', $.size({title: 'html', showFiles: true})))
+  // Minify any HTML
+  // .pipe($.if('*.html', $.htmlmin({
+  //   removeComments: true,
+  //   collapseWhitespace: true,
+  //   collapseBooleanAttributes: true,
+  //   removeAttributeQuotes: true,
+  //   removeRedundantAttributes: true,
+  //   removeEmptyAttributes: true,
+  //   removeScriptTypeAttributes: true,
+  //   removeStyleLinkTypeAttributes: true,
+  //   removeOptionalTags: true
+  // })))
+  // Output files
+  .pipe($.if('*.html', $.size({
+    title: 'html',
+    showFiles: true
+  })))
     .pipe(gulp.dest('dist'));
 });
 
 // Clean output directory
-gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
+gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {
+  dot: true
+}));
 
 // Watch files for changes & reload
 gulp.task('serve', ['scripts', 'styles'], () => {
@@ -182,7 +201,7 @@ gulp.task('serve', ['scripts', 'styles'], () => {
   gulp.watch(['app/images/**/*'], reload);
 });
 
-gulp.task('test', function (done) {
+gulp.task('test', function(done) {
   var karma = new Karma({
     configFile: __dirname + '/karma.conf.js',
     singleRun: false
@@ -207,8 +226,7 @@ gulp.task('serve:dist', ['default'], () =>
 // Build production files, the default task
 gulp.task('default', ['clean'], cb =>
   runSequence(
-    'styles',
-    ['lint', 'html', 'scripts', 'images', 'copy'],
+    'styles', ['lint', 'html', 'scripts', 'images', 'copy'],
     cb
   )
 );
